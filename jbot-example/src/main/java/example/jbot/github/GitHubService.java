a6f2030c33d08ef7cecdb4fbc0077169592c20eb
Artem Gubanov <tema.gubanov@gmail.com>
2018-06-21 14:48:55 +0300

    add test data, hack for github api

--- jbot-example/src/main/java/example/jbot/github/GitHubService.java
+++ jbot-example/src/main/java/example/jbot/github/GitHubService.java
@@ -2,12 +2,29 @@ package example.jbot.github;
 
 import java.io.IOException;
 
+/**
+ * test data:
+ *  host: "https://github.com"
+ *  repo: "agubanov/jbot"
+ *  token: "f719bfabfb5d87e216029873b23dfc2f23966820"
+ *  title: "pr from bot"
+ *  from: "test-branch"
+ *  to: "master"
+ *
+ *  param string:
+ *  	"https://github.com", "agubanov/jbot", "f719bfabfb5d87e216029873b23dfc2f23966820", "pr from bot", "test-branch", "master"
+ */
 public class GitHubService {
 	
 	public static String createGitHubPullRequestAndGetReviewLink(String host, String repo, String token,
 														   String title, String from, String to) throws IOException {
 		GitHubAPI api = GitHubAPI.build(host, repo, token);
 		GitHubPullRequest pullRequest = api.createPullRequest(title, from, to);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		GitHubCombinedStatus status = api.getStatus(pullRequest);
 		return status.getReviewUrl();
 	}
