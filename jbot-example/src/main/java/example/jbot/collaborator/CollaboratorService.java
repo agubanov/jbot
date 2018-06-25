e44cb6ccfef09df7c750f7c9a992aa4f31719e68
Andrey Ivashchenko <Andrey.Ivashchenko@smartbear.com>
2018-06-25 13:18:52 +0300

    added new method for JSON communication with Collaborator

--- jbot-example/src/main/java/example/jbot/collaborator/CollaboratorService.java
+++ jbot-example/src/main/java/example/jbot/collaborator/CollaboratorService.java
@@ -1,7 +1,9 @@
 package example.jbot.collaborator;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import example.jbot.collaborator.dto.LoginRequestArgs;
 import example.jbot.collaborator.dto.LoginRequestDto;
+import example.jbot.collaborator.dto.LoginResponseDto;
 import example.jbot.github.GitHubApiClient;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
@@ -10,7 +12,6 @@ import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.StringRequestEntity;
 
 import java.io.IOException;
-import java.net.URL;
 
 /**
  * General class for communication with collaborator
@@ -18,36 +19,83 @@ import java.net.URL;
 public class CollaboratorService {
 
     private static final String JSON_API_ENDPOINT = "/services/json/v1";
+    private static String serverUrl = "http://collab.aus.smartbear.com";
     private HttpClient client = new HttpClient();
     private HttpMethod method;
     private GitHubApiClient.Method methodType;
     public enum Method { GET, PUT, POST, DELETE }
 
+
+    /**
+     * Get Auth token from collaborator
+     * @param login - login
+     * @param password - password
+     * @param collabHost - Collaborator host
+     * @return
+     */
     public String login(String login, String password, String collabHost) {
+
+        LoginRequestDto request = new LoginRequestDto();
+        request.setArgs(new LoginRequestArgs(login, password));
+        StringBuilder debugMessage = new StringBuilder();
+        LoginResponseDto response = request(request, LoginResponseDto.class, debugMessage);
+        if(response != null){
+            return response.getResult().getLoginTicket();
+        }
+        else{
+            return debugMessage.toString();
+        }
+    }
+
+    /**
+     * General method for communication with Collaborator
+     * @param requestObject
+     * @param responseObjClass
+     * @param message
+     * @param <S>
+     * @param <T>
+     * @return
+     */
+
+    public <S, T> T request(S requestObject, Class<T> responseObjClass, StringBuilder message){
         try {
 
-            LoginRequestDto request = new LoginRequestDto();
             ObjectMapper mapper = new ObjectMapper();
-            String jsonInString = mapper.writeValueAsString(request);
+            String jsonInString = mapper.writeValueAsString(requestObject);
 
-            URL serverURL = new URL("http://collab.aus.smartbear.com");
             this.method = new PostMethod();
-            PostMethod method = new PostMethod("http://collab.aus.smartbear.com"+JSON_API_ENDPOINT);
-            //((PostMethod) method).getParams().setParameters();
+            PostMethod method = new PostMethod(serverUrl + JSON_API_ENDPOINT);
             StringRequestEntity requestEntity = new StringRequestEntity(
-                    " [ {\"command\" : \"SessionService.getLoginTicket\", \"args\":{\"login\": \"aivashchenko\", \"password\": \"<my-password>\"}} ]",
+                    " [ " + jsonInString + " ]",
                     "application/json",
                     "UTF-8");
             method.setRequestEntity(requestEntity);
             int statusCode = client.executeMethod(method);
+
             if(statusCode == HttpStatus.SC_OK){
                 String resStr = new String(method.getResponseBody(), "UTF-8");
-                return resStr;
+                //save message for debug info
+                message.append(resStr);
+                resStr = resStr.replace('[', ' ');
+                resStr = resStr.replace(']', ' ');
+                T result = mapper.readValue(resStr, responseObjClass);
+                return result;
             }
 
         }catch (IOException ex){
-            return "Exception " + ex.getMessage();
+            message.append("Exception " + ex.getMessage());
+            return null;
         }
-        return "";
+
+        return null;
+
+    }
+
+    public static String getServerUrl() {
+        return serverUrl;
+    }
+
+    public static void setServerUrl(String serverUrl) {
+        CollaboratorService.serverUrl = serverUrl;
     }
 }
